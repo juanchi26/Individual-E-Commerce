07051fe21bfb34640ba17b87c66765cfc9d5ed29
Juanchi26 <juanchi.diaz@hotmail.com>
2022-08-16 22:27:54 -0300

    cambios product login

---
+++ js/login.js
@@ -0,0 +1,17 @@
+let email = document.getElementById("email")
+let pass = document.getElementById("pass")
+let boton = document.getElementById("boton")
+
+
+
+
+boton.addEventListener(`click`, function () {
+    let expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    let correo = expReg.test(email.value);
+    if (pass.value.length > 0 && correo== true ) {
+        window.location.href = "mercado.html";
+    }
+    else {
+        alert("Los campos no pueden estar vacios");
+    }
+})
---
+++ js/products.js
@@ -0,0 +1,32 @@
+let url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(url).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            datos = resultObj.data
+            let contenido = "";
+            for (let articulo of datos.products) {
+
+                contenido += `
+                <div onclick="setCatID(${datos.catID})" class="list-group-item list-group-item-action cursor-active">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${articulo.image}" alt="${articulo.description}" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">${articulo.name} - ${articulo.currency} ${articulo.cost}</h4>
+                                <small class="text-muted">${articulo.soldCount} Vendidos</small>
+                            </div>
+                            <p class="mb-1">${articulo.description}</p>
+                        </div>
+                    </div>
+                </div>
+                `
+            }
+
+            document.getElementsByClassName("container")[2].innerHTML = contenido;
+        }
+
+    })
+})
