d756197e9c15b78a4730ead1928d3385d8387fa2
Juanchi26 <juanchi.diaz@hotmail.com>
2022-08-31 17:29:45 -0300

    test

--- js/products.js
+++ js/products.js
@@ -1,22 +1,73 @@
 ID = localStorage.getItem("catID")
+
 let url = `https://japceibal.github.io/emercado-api/cats_products/${ID}.json`
+let datos = [];
+let min = undefined
+let max = undefined
+let buscar = undefined
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(url).then(function (resultObj) {
         if (resultObj.status === "ok") {
             datos = resultObj.data
-            ListaAutos();
+            ListaArticulos();
+            producto();
         }
 
     })
+
+    document.getElementById("rangeFilterCount").addEventListener("click", function () {
+        min = parseInt(document.getElementById("rangeFilterCountMin").value);
+        max = parseInt(document.getElementById("rangeFilterCountMax").value);
+
+        if ((min != undefined) && (min != "") && (parseInt(min)) >= 0) {                              // evita que el campo quede NaN y sea o undefined o un numero
+            min = parseInt(min);
+        }
+        else {
+            min = undefined;
+        }
+
+        if ((max != undefined) && (max != "") && (parseInt(max)) >= 0) {
+            max = parseInt(max);
+        }
+        else {
+            max = undefined;
+        }
+
+        ListaArticulos()
+    })
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function () {              // limpia lo campos de min y max los setea en undefined y carga los articulos de nuevo
+
+        document.getElementById("rangeFilterCountMin").value = "";
+        document.getElementById("rangeFilterCountMax").value = "";
+
+        max = undefined;
+        min = undefined;
+
+        ListaArticulos();
+    });
+
+
+
+    document.getElementById("buscar").addEventListener("input", function () {
+        buscar = document.getElementById("buscar").value.toLowerCase();
+        ListaArticulos();
+    })
+
 })
 
 
-function ListaAutos() {
+function ListaArticulos() {
     let contenido = "";
     for (let articulo of datos.products) {
+        articulo.cost = parseInt(articulo.cost);
 
-        contenido += `
+        if ((min == undefined && max == undefined) || (articulo.cost >= min && articulo.cost <= max) ||          // filtra por precio
+            (min == undefined && articulo.cost <= max) || (articulo.cost >= min && max == undefined)) {
+
+            if (buscar == undefined || buscar == "" || articulo.name.toLowerCase().includes(buscar) || articulo.description.toLowerCase().includes(buscar)) {   // filtra por nombre o descripcion tipeados
+                contenido += `
                 <div onclick="setCatID(${datos.catID})" class="list-group-item list-group-item-action cursor-active">
                     <div class="row">
                         <div class="col-3">
@@ -32,9 +83,17 @@ function ListaAutos() {
                     </div>
                 </div>
                 `
+            }
+        }
     }
 
     document.getElementsByClassName("container")[2].innerHTML = contenido;
 }
 
 // recorre un array y los dibuja con los parametros seleccionados
+
+producto = () => document.getElementById(`articulos`).innerHTML = `Verás aquí todos los productos de la categoría <strong>${datos.catName}</strong>`
+
+// agrega en el parrafo el nombre de la categoria
+
+
