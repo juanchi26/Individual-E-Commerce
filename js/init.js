34138584fa6962de35111740b4b3d411692ee192
Juanchi26 <juanchi.diaz@hotmail.com>
2022-09-15 19:18:28 -0300

    test cerrar sesion

--- js/init.js
+++ js/init.js
@@ -7,47 +7,53 @@ const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 
-let showSpinner = function(){
+let showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-let hideSpinner = function(){
+let hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-let getJSONData = function(url){
-    let result = {};
-    showSpinner();
-    return fetch(url)
+let getJSONData = function (url) {
+  let result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-  let mail= localStorage.getItem("email")
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let mail = localStorage.getItem("email")
   localStorage.setItem("sesion", `Iniciar Sesion`)
   let getsesion = localStorage.getItem("sesion")
-      
-  if(mail){
 
-  document.getElementById("navbarNav").innerHTML = `<div class="collapse navbar-collapse" id="navbarNav">
+
+
+
+  if (mail) {
+
+    document.getElementById("navbarNav").innerHTML = `<div class="collapse navbar-collapse" id="navbarNav">
   <ul class="navbar-nav w-100 justify-content-between">
     <li class="nav-item">
       <a class="nav-link" href="mercado.html">Inicio</a>
@@ -61,11 +67,14 @@ document.addEventListener("DOMContentLoaded", function(){
     <li class="nav-item" id="mail">
       <a class="nav-link">${mail}</a>
     </li>
+    <li class="nav-item">
+      <a id="cierreSesion" class="nav-link">Cerrar sesion</a>
+    </li>
   </ul>
 </div>`
-      }else{
-  
-      document.getElementById("navbarNav").innerHTML = `<div class="collapse navbar-collapse" id="navbarNav">
+  } else {
+
+    document.getElementById("navbarNav").innerHTML = `<div class="collapse navbar-collapse" id="navbarNav">
   <ul class="navbar-nav w-100 justify-content-between">
     <li class="nav-item">
       <a class="nav-link" href="mercado.html">Inicio</a>
@@ -81,6 +90,18 @@ document.addEventListener("DOMContentLoaded", function(){
     </li>
   </ul>
 </div>`
-      }
- 
+  }
+
+  let cerrarSesion = document.getElementById("cierreSesion")
+
+  cerrarSesion.addEventListener("click", function () {
+
+
+    localStorage.removeItem("email")
+    location.reload()
+
+
+  })
+
 })
+
